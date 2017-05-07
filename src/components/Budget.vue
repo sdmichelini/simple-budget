5091725b5b6513d0e46ff50b64bfa0c2838f77da
Stephen Michelini <sdmichelini@gmail.com>
2017-05-07 18:19:54 -0400

    Added Budget and BudgetItem

---
+++ src/components/Budget.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id='budget'>
+        <h1>Budget</h1>
+        <h2>Income</h2>
+        <h2>Expenses</h2>
+        <div class="list-group">
+            <template v-for="item in expenseItems">
+                <budget-item v-bind:item="item"></budget-item>
+            </template>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'budget',
+  data: () => {
+    return {
+      expenseItems: [
+          {name: 'Apartment', amount: 1400.0, category: 'Housing'},
+          {name: 'Car Payment', amount: 400.0, category: 'Car'}
+      ]
+    }
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/BudgetItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li class="list-group-item">
+      <strong>{{ item.name }}</strong>  - {{ item.category }}
+      <div id="amount">${{ item.amount }}</div>
+    </li>
+</template>
+<script>
+export default {
+  name: 'budget-item',
+  props: ['item']
+}
+</script>
+<style>
+#amount{
+  margin-left: auto;
+}
+</style>
